4a4cb2de73451b60ae845d8c85ff5fc9a579c6a3
Ben Unsworth <ben.unsworth1@gmail.com>
2015-05-19 11:22:12 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # R-Census-Heatmap
-A heatmap of citizen reported health from the 2011 Census
+
+Using the RSocrata package to load data into R as a data frame and making a heatmap
+
+A heatmap of citizen reported health from the 2011 Census for wards in Bristol
+
+You can find the source data at https://opendata.bristol.gov.uk/Health/2011-Census-health/bdwv-2hn9
+
+There is also a great write-up on the RSocrata package from the City of Chicago's Digital Team http://digital.cityofchicago.org/index.php/rsocrata/
+RSocrata is available on CRAN http://cran.stat.ucla.edu/web/packages/RSocrata/index.html
---
+++ bristol_census_heatmap.r
@@ -0,0 +1,28 @@
+# How to make a simple heatmap on citizen reported health, using 2011 Census data from Bristol City Council's Socrata platform
+
+library("pacman")  # Load package manager, you might need to install this package first
+p_load(RSocrata,plyr,RColorBrewer)  # Use Pacman to load required packages, you might need to install these first
+
+# Next job is load the data from opendata.bristol.gov.uk as a dataframe
+
+census <- read.socrata("https://opendata.bristol.gov.uk/Health/2011-Census-health/bdwv-2hn9") 
+
+# You can check the data looks right with: head(bristol.census)
+# There are a few data cleaning/munging jobs before we can visualise
+
+census <- census[order(census$Very.good.health.rate),]  # Ordered the data by Very.good.health.rate column
+
+census <- census[,-c(1,2,3,4,6,8,10,12,14)]  # Delete the columns that we don't need - TIP... columns start at "1" not "0" in the R Language
+
+row.names(census) <- census$Names  # R automatically numbers rows, this replaces the numbers with values from the column "Names"
+census <- rename(census, c("Very.good.health.rate"="Very Good", "Good.health.rate"="Good",
+ "Fair.health.rate"="Fair", "Bad.health.rate"="Bad", 
+ "Very.bad.health.rate"="Very bad"))  # Using the plyr library's "rename" function to give the columns human readable names
+
+census_matrix <- data.matrix(census)  # To make a heatmap in R we need the data in a matrix
+
+# As you can see, most of the code ^^ is about getting our data into the right shape for the visualisation
+# The final task is to use R's built in heatmap function and the RColorBrewer library to add some nice blue shades
+
+health_heatmap <- heatmap(census_matrix, Rowv=NA, Colv=NA, col=brewer.pal(9,"Blues"), scale="column", margins=c(5,10), main="Bristol, Resident Reported Health by Ward - 2011 Census") 
+
